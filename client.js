7c2c635230e8842391cfaa3b02aa3cbd9e521ab6
jeff9014223 <9014223@student.zadkine.nl>
2023-09-30 14:54:20 +0200

    *

---
+++ client.js
@@ -0,0 +1,7 @@
+function kleur_aanpassen(kleur) {
+    let title = document.getElementById("title");
+    title.style.color = kleur;
+    title.innerText = "Inhoud aangepast";
+};
+
+kleur_aanpassen("red")
